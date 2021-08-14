c3be5b78f08866bbb184278410bb3520c7516473
Hiroki Yokoyama <hiroking.y@gmail.com>
2021-08-14 22:55:13 +0900

    8回目 Commit

--- app.py
+++ app.py
@@ -8,14 +8,27 @@ from flask.globals import session
 app = Flask(__name__)
 app.secret_key = 'yokoyama' # secret key
 
-member_list2 = []
+member_list =[] #global変数
+
 global_ulfnum = 0
 global member_vote_list
 
 @app.route('/') # メインページ
 def main():
-   
-     return render_template('main.html')
+    return render_template('main.html')
+
+
+@app.route("/index",methods=["post"])
+def post():
+
+    if "username" not in session:
+        session["username"] = request.form["username"]
+
+    
+    myname = session.get('username')
+    member_list.append(request.form["username"]) 
+  
+    return render_template('member_list.html', member_list =member_list , val = 0 , myname = myname)
 
 @app.route('/reset1',methods=["post"]) # リセット
 def reset1():
@@ -24,25 +37,13 @@ def reset1():
    if "username" in session:  # セッション情報があれば削除
         session.pop('username', None)
 
-   member_list2 = session.get('member_list2')
-   member_list2.clear
-   member_list2 =[]
+   member_list =[]
    global_ulfnum = 0
-    
-   return render_template('main.html',member_list2 = member_list2)
-    
-@app.route("/index",methods=["post"])
-def post():
 
-    if "username" not in session:
-        session["username"] = request.form["username"]
-        
-    myname = session.get('username')
-    member_list2.append(request.form["username"]) 
     
-    session['member_list2'] = member_list2 #session に格納
-  
-    return render_template('member_list.html', member_list2 =member_list2 , val = 0 , myname = myname)
+   return render_template('main.html',member_list = member_list)
+    
+
 
 @app.route("/prepare",methods=["post"]) # 開始準備確認
 def odai_warifuri(): 
@@ -52,7 +53,6 @@ def odai_warifuri():
 
     
     myname = session.get('username')
-    member_list2 = session.get('member_list2')
     
     if myname is None:
         print("ユーザ名がNoneになってしまっています")
@@ -60,14 +60,14 @@ def odai_warifuri():
     else:
         flg_none = '0'
     
-    listsize = len(member_list2)
+    listsize = len(member_list)
     global_ulfnum = random.randint(1,listsize) #ここでウルフを決定する.
     member_vote_list =[0] * listsize #投票結果をリセット
      #デバッグモード print("投票数値リスト→",member_vote_list) 
-     #デバッグモード print([member_list2], listsize ,global_ulfnum,member_list2[global_ulfnum-1]) 
-    print("ウルフNO → ",global_ulfnum,"ウルフ名 → ",member_list2[global_ulfnum-1]) 
+     #デバッグモード print([member_list], listsize ,global_ulfnum,member_list[global_ulfnum-1]) 
+    print("ウルフNO → ",global_ulfnum,"ウルフ名 → ",member_list[global_ulfnum-1]) 
 
-    return render_template('member_list_prepare.html',member_list2 =member_list2, myname = myname , flg_none = flg_none )
+    return render_template('member_list_prepare.html',member_list =member_list, myname = myname , flg_none = flg_none )
 
 
  ## お題配信する
@@ -75,20 +75,23 @@ def odai_warifuri():
 def odai_haishin():
      
      myname = session.get('username')
+
      print("ウルフNo→→　　",global_ulfnum)
      print("request.form['action'] →→　　",int(request.form['action']))
      if int(request.form['action']) == global_ulfnum: #ウルフのときの処理
             wordtheme = "ウルフ"
-            return render_template('odai.html',wordtheme = wordtheme,member_list2 =member_list2,myname = myname)
+            return render_template('odai.html',wordtheme = wordtheme,member_list =member_list,myname = myname)
     
      else:                                       #市民のときの処理
             wordtheme = "市民"
-            return render_template('odai.html',wordtheme = wordtheme,member_list2 =member_list2,myname = myname)
+            return render_template('odai.html',wordtheme = wordtheme,member_list =member_list,myname = myname)
 
 ## 投票結果 
 @app.route('/vote', methods=['POST']) 
 def vote_result():
+    
     myname = session.get('username')
+
     global ulf_of_name
     votenumber = int(request.form.get('sel'))
     print("投票",votenumber)  #デバッグモード
@@ -98,22 +101,22 @@ def vote_result():
     member_vote_list[votenumber-1] = member_vote_list[votenumber-1] + 1
     print("投票数値リスト(更新)→",member_vote_list)  #デバッグモード
 
-    ulf_of_name = member_list2[global_ulfnum-1] #ウルフの人の名前を代入
+    ulf_of_name = member_list[global_ulfnum-1] #ウルフの人の名前を代入
     
-    return render_template('vote_result.html',member_list2 =member_list2,member_vote_list = member_vote_list,ulf_of_name = ulf_of_name,myname = myname)
+    return render_template('vote_result.html',member_list =member_list,member_vote_list = member_vote_list,ulf_of_name = ulf_of_name,myname = myname)
 
 ## メンバー一覧ページ　
 @app.route('/memberlist')
 def load_member_list():
     
     myname = session.get('username')
-    member_list2 = session.get('member_list2')
-    #print("member_list2===> " ,member_list2)
-    return render_template('member_list.html',member_list2 =member_list2,myname = myname)
+    #print("member_list===> " ,member_list)
+    return render_template('member_list.html',member_list =member_list,myname = myname)
 
 ## お題割り振りページ　
 @app.route('/memberlist_prepare')
 def memberlist_prepare():
+
     myname = session.get('username')
     
     if myname is None:
@@ -122,13 +125,14 @@ def memberlist_prepare():
     else:
         flg_none = '0'
 
-    return render_template('member_list_prepare.html',member_list2 =member_list2,myname = myname,flg_none = flg_none)
+    return render_template('member_list_prepare.html',member_list =member_list,myname = myname,flg_none = flg_none)
 
 ## 投票結果　
 @app.route('/result')
 def result():
     myname = session.get('username')
-    return render_template('vote_result.html',member_list2 =member_list2,member_vote_list = member_vote_list,ulf_of_name = ulf_of_name,myname = myname)
+
+    return render_template('vote_result.html',member_list =member_list,member_vote_list = member_vote_list,ulf_of_name = ulf_of_name,myname = myname)
 
 ## 利用規約
 @app.route('/terms') 
